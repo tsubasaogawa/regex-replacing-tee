b2266803d1d2d17cb4119bfdabecbf65ad668a2c
tsubasaogawa <hurikake@gmail.com>
2023-11-14 23:13:42 +0900

    feat: make it possible to use toml

--- main.go
+++ main.go
@@ -5,10 +5,29 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"regexp"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+)
+
+const (
+	CONF_FILE = "config.toml"
+)
+
+type (
+	config struct {
+		Rules map[string]rule
+	}
+	rule struct {
+		From string
+		To   string
+	}
 )
 
 func main() {
+	loadConf()
 	r := regexp.MustCompile(`ogawa`)
 	f, err := os.OpenFile("/tmp/ogawa.log", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
@@ -25,4 +44,46 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func loadConf() {
+	c := fmt.Sprintf("%s/%s", getAppDir(), CONF_FILE)
+	var conf config
+	if exists(c) {
+		if _, err := toml.DecodeFile(c, &conf); err != nil {
+			log.Fatal(err)
+		}
+	}
+	fmt.Printf("%s: %#v\n", c, conf.Rules)
+}
+
+func getAppDir() string {
+	d, err := os.Executable()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !isGoRun(d) {
+		return filepath.Dir(d)
+	}
+
+	d, err = os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !exists(filepath.Join(d, "main.go")) {
+		log.Fatal("Please `go run` in the script directory")
+	}
+
+	return d
+}
+
+func isGoRun(_path string) bool {
+	return strings.Contains(_path, "go-build")
+}
+
+func exists(_path string) bool {
+	_, err := os.Stat(_path)
+	return err == nil
 }
